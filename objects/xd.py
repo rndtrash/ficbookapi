599bb581610095c363fb9638a1cc34f660f457fd
Ivan Kuzmenko <kuzmenko.ivan2002@yandex.com>
2020-03-20 11:09:28 +0300

    Use 127.0.0.1 instead of localhost

--- objects/xd.py
+++ objects/xd.py
@@ -7,7 +7,7 @@ ficPath = "readfic"
 
 version = "0.1a"
 port = int(os.getenv('VCAP_APP_PORT', os.getenv('PORT', 8080)))
-host = os.getenv('VCAP_APP_HOST', 'localhost')
+host = os.getenv('VCAP_APP_HOST', '127.0.0.1')
 
 threads = 16
 requests = 0
